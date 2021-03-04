5cfa342e5bc16ea163425df88ab837ee47e7b778
Cub Olson <cub.olson@gmail.com>
2021-03-04 15:33:40 -0500

    last save

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", async () => {
     try {
       const res = await fetch("http://localhost:8080/tweets");
-      const { tweets } = await res.json();
+        if (res.status === 401) {
+            res.redirect('/log-in');
+            return
+        } else {
+            const { tweets } = await res.json();
+        }
       console.log(tweets);
     } catch (e) {
       console.error(e);
